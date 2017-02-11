c520ae00f4ad2803280344b5df2c172d64c8a164
magnlar <cmr.larsson@gmail.com>
2017-02-11 15:12:46 +0100

    Aadsa

---
+++ Hello/Bananas.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Bananas here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bananas
+{
+    private static final String tehMessage = "Mucho bananas";
+
+    
+    public static String getMess()
+    {
+        return tehMessage;
+    }
+}
